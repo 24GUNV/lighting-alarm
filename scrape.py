f160b6e392027c1629908654ccf80ac0a43a4a2a
24GUNV <24progun@gmail.com>
2023-05-21 16:13:31 +0700

    Updated scrape.py
    Checks for refreshes between cycles + Fixed logging issues

--- scrape.py
+++ scrape.py
@@ -5,31 +5,64 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time, logging
 
-# web address for data scraping
+# Web address for data scraping
 url = "https://lxapp.weatherbug.net/v2/lxapp_impl.html?lat=13.66744&lon=100.64206&v=1.2.0"
 
-# logger setup
-logging.basicConfig(filename='log.txt', encoding='utf-8', level=logging.DEBUG)
+# Logger setup
+logging.basicConfig(filename='log.txt',
+		    		filemode='a',
+                    format='%(asctime)s %(name)s %(levelname)s- %(message)s',
+		     		encoding='utf-8',
+				    datefmt='%H:%M:%S',
+					level=logging.NOTSET)
+logging.disable(logging.DEBUG)
+logging.getLogger("selenium.webdriver.common.selenium_manager").disabled = True
+logging = logging.getLogger("Lightning-Alarm")
 
-# (comment)
+
+# Configuring webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-# (comment)
+# Setting up url to be scraped
 driver = webdriver.Chrome('chromedriver',options=chrome_options)
 driver.get(url)
 page = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "sparkDistanceTxt")))
 
-iterations = 10
-for i in range(10):
-	soup = BeautifulSoup(driver.page_source, "html.parser")
+logging.info("Starting logging session")
 
+try:
+	while True:
+		# Checks whether the webpage is still updating
+		originalPage = BeautifulSoup(driver.page_source, "html.parser")
+		distance=originalPage.find_all('span', class_="closest-strike-distance")[0].text
 
-	distance=soup.find_all('span', class_="closest-strike-distance")
-	print(distance)
-	
-	time.sleep(5)
+		time.sleep(1)
 
-driver.close()
+		newPage = BeautifulSoup(driver.page_source, "html.parser")
+		newDistance = newPage.find_all('span', class_="closest-strike-distance")[0].text
+
+		while newDistance != distance:
+			originalPage = newPage
+			distance=newDistance
+
+			time.sleep(1)
+			newPage = BeautifulSoup(driver.page_source, "html.parser")
+			newDistance = newPage.find_all('span', class_="closest-strike-distance")[0].text
+		else:
+			distance = newDistance
+
+		# Logs the distance into the log
+		logging.info(f"Distance: {distance}")
+		
+		time.sleep(300) # Waits for 5 mins until the next cycle
+		
+except KeyboardInterrupt:
+	logging.info("User ended program")
+except:
+	logging.fatal("An Error has occured")
+finally:
+	logging.info("Program ended")
+	driver.close()
